9f47b82a3d53d6804ddffade816f01d9067d4651
_Reiju_ <sakurapleasure0@gmail.com>
2017-03-18 16:18:09 +0900

    Fixed some issues

--- index.js
+++ index.js
@@ -24,11 +24,14 @@ var WebClient = require("@slack/client").WebClient;
 var RTM_EVENTS = require("@slack/client").RTM_EVENTS;
 var webshot = require("webshot");
 var fs = require("fs");
+var Entities = require("html-entities").AllHtmlEntities;
 
 var token = process.env.SLACK_API_TOKEN || "";
 var rtm = new RtmClient(token, {logLevel: "normal"});
 var web = new WebClient(token);
 rtm.start();
+
+entities = new Entities();
 
 var prefix="reiju:"
 var regex = /.*?\<(.+?)[|\>].*/
@@ -36,26 +39,30 @@ var regex = /.*?\<(.+?)[|\>].*/
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 	//var message = {text: "reiju: <https://google.com>"};
 	console.log(message.text);
+	if(!message.text) return;
 	try {
 		url = message.text.match(regex);
 	} catch(e) {
 		//web.chat.postMessage(message.channel, "Error: 文字列関係がア！", {as_user: true}, function(err, res) {
-			console.log(err, res);
+			console.log(e);
 		//});
 	}
 	if (url) {
-		console.log(url[0].indexOf("reiju:"));
+		//console.log(url[0].indexOf("reiju:"));
 		if(url[0].indexOf("reiju:") != -1) {
-			console.log("match! ", url[1]);
+			//console.log("match! ", url[1]);
 			var filePath = __dirname+"/img/"+Date.now()+".png";
-			var urlstr = url[1];
+			var urlstr = entities.decode(url[1]);
 			
 			const promise = new Promise((resolve, reject) => {
-				webshot(url[1], filePath, {shotSize: 
-{width: "all", height: "all"}, renderDelay: 3*1000}, function(err) {
+				webshot(urlstr, filePath, {
+					shotSize: {width: "all", height: "all"}, renderDelay: 3*1000,
+					userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+					defaultWhiteBackground: true, quality: 100, timeout: 15*1000
+				}, function(err) {
 					if(err) {
 						web.chat.postMessage(message.channel, "PhantomJSが限界になった(error)", {as_user: true}, function(err, res) {
-							console.log(err, res);
+							if(err) console.log(err);
 						});
 						console.log(err);
 						reject();
@@ -65,7 +72,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 			});
 			promise.then(() => web.files.upload("phantom: "+urlstr, {file: fs.createReadStream(filePath), channels: message.channel, 
 title: "PhantomJS: "+urlstr}, function handleStreamFileUpload(err, res) {
-				console.log(err, res);
+				if(err) console.log(err);
 			}));
 		}
 	}
